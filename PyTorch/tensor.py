43d4991c15c13003ac0965ccda65b7be8fc3d711
JuhwanLim <juhwan1120@naver.com>
2024-12-10 15:30:03 +0900

    tensor의 기본연산

---
+++ PyTorch/tensor.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+
+# list_data = [ [10,20], [30,40] ]
+
+# tensor1 = torch.Tensor(list_data)
+
+# print(tensor1)
+# print(f"tensor type: {type(tensor1)}, tensor shape: ({tensor1.shape}")
+# print(f"tensor type: {tensor1.dtype}, tensor shape: ({tensor1.device}")
+
+# tensor3 = torch.rand(2,2)
+# print(tensor3)
+
+# tensor4 = torch.randn(2,2)
+# print(tensor4)
+
+# numpy_from_tensor = tensor4.numpy()
+# print(numpy_from_tensor)
+
+# tensorA = torch.Tensor([[1,2,3], [4,5,6]])
+# tensorB = torch.Tensor([[7,8,9], [10,11,12]])
+
+# print(tensorA[0])
+# print(tensorA[:, 2:])
+# print(tensorA[1])
+# print(tensorB[0:2, 0:-1])
+# print(tensorB[-1,-2])
+# print(tensorB[..., -1])
+
+# tensor5 = tensorA.mul(tensorB)
+# tensor9 = tensorA.matul(tensorB,view(3, 2)) 
